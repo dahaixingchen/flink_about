a72989a0501aab715a91f1a7b36d116e260d168d
chengfei <xingchendahai_fei@163.com>
2019-11-17 09:18:56 +0800

    flink算子

---
+++ flink-common/src/main/java/com/chengfei/kafka/GetProducer.java
@@ -0,0 +1,17 @@
+package com.chengfei.kafka;
+
+import org.apache.kafka.clients.producer.Producer;
+
+/**
+ * @ClassName: GetProducer
+ * @Description: TODO
+ * @Author chengfei
+ * @Date 2019/11/15 10:52
+ * @Version 1.0
+ **/
+public class GetProducer {
+    public Producer getProducer(){
+
+        return null;
+    }
+}
---
+++ flink-common/src/test/java/com/chengfei/kafka/producer/SendMessage2Partition.java
@@ -0,0 +1,12 @@
+package com.chengfei.kafka.producer;
+
+/**
+ * @ClassName: SendMessage2Partition
+ * @Description: TODO
+ * @Author chengfei
+ * @Date 2019/11/15 13:53
+ * @Version 1.0
+ **/
+public class SendMessage2Partition {
+
+}
